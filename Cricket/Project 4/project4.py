bd76efb469e520b38966476283c671b12830e047
Cricket Bergner <cricketbergner@gmail.com>
2026-04-22 19:38:14 -0700

    Create project4.py (beginning of final draft file)
    final draft beginning
    
    "If you know the enemy and know yourself, you need not fear the result of a hundred battles. If you know yourself but not the enemy, for every victory gained you will also suffer a defeat. If you know neither the enemy nor yourself, you will succumb in every battle."
    - Sun Tzu, _The Art of War_

---
+++ Cricket/Project 4/project4.py
@@ -0,0 +1,12 @@
+'''
+Filename: project4.py
+Written by: Cricket Bergner
+Date: 04/22/2026
+'''
+
+# import libraries
+import numpy as np
+from matplotlib import pyplot as plt
+import random as ra
+from matplotlib.animation import FuncAnimation
+from mpl_toolkits.mplot3d import Axes3D
