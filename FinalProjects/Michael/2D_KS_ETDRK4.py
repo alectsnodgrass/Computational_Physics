316de24d02ea8180df1055f9d2198765d9f1e56c
michaeldpotts <michaeldpotts@outlook.com>
2026-04-29 12:20:42 -0500

    Simulation is working now
    There were two major problems that were fixed today
    1) There was a strange graininess to the simulation that was interfering with the plots. This was fixed by increasing the number of Fourier modes.
    
    2) Fixed the problem with the large uniform divergence.  ETDRK4 is a pseudo spectral method, so the numerical solution is only calculating the amplitudes of Fourier modes over time. For the 2D Kuramoto-Sivashinksy equation, the (0,0) mode (the average of the solution) should not evolve in time. This is an issue because this method does computations that risk catastrophic cancellation near (0,0). Even the slightest error in the evolution of the (0,0) mode was able to accumulate. This caused that large uniform background. I fixed this by forcing the (0,0) mode to be constant in time, and I got the simulation working.
    
    3) Besides that, the code was organized in a nicer way so that clear logical chunks are present, and the various parameters/things worth tweaking between runs are at the top of the program now.

--- FinalProjects/Michael/2D_KS_ETDRK4.py
+++ FinalProjects/Michael/2D_KS_ETDRK4.py
@@ -2,93 +2,138 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation, FFMpegWriter
 
-# Grid
-N = 128
-L = 32.0
-t_steps = 1000
-x = np.linspace(0, L, N, endpoint=False)
-y = np.linspace(0, L, N, endpoint=False)
+# SIMULATION PARAMETERS ==================================================================================================================
+#Control animation saving
+save_anim = False
+
+#color option for animation
+color = 'viridis'
+
+#nplt = number of steps stored in the animation. nplt = 1 means save all. larger means save less
+nplt = 1
+
+# N = number of points along a spatial dimension
+N = 300
+# size = length along the spatial dimensions
+size = 30
+
+#Tmax = maximum time
+Tmax = 600
+
+# h = time step size
+h = 0.125 
+
+#Verbosity controls how often the program reports progress
+verbosity = int(Tmax/(10*h))
+
+# initial condtions
+x = size*np.arange(0,N)/N
+y = size*np.arange(0,N)/N
+
+# random noise ICS
+u0 = 0.01*np.random.randn(len(x), len(y))
+
+# single bump IC
+u0 = np.zeros((len(x), len(y)))
+u0[len(x)//2, len(y)//2] = 2
+
+# CREATE THE SPATIAL GRID ==================================================================================================================
+
 X, Y = np.meshgrid(x, y, indexing='ij')
 
-# Fourier modes
-kx = 2*np.pi * np.fft.fftfreq(N, d=L/N)
-ky = 2*np.pi * np.fft.fftfreq(N, d=L/N)
+# CREATE THE FOURIER MODES AND L OPERATOR ==================================================================================================================
+dx = x[1] - x[0]
+dy = y[1] - y[0]
+
+kx = np.fft.fftfreq(N, d=dx) * 2*np.pi
+ky = np.fft.fftfreq(N, d=dy) * 2*np.pi
+
 Kx, Ky = np.meshgrid(kx, ky, indexing='ij')
 
 K2 = Kx**2 + Ky**2
 
-L = K2 - K2**2
+L = 0.5*(K2 - K2**2)
 
-# initial condtion and fourier transform of u0
-u0 = 0.1*np.random.randn(N, N)
-v0 = np.fft.fft2(u0)
-
-# ETDRK4 coefficients ===================================
-h = 0.25 #time step size
-
-# operators 
+# ETDRK4 COEFFICIENTS =========================================================================================================
+t_steps = int(Tmax/h)
+# Operator Exponentials 
 E = np.exp(h*L)
 E2 = np.exp(0.5*h*L)
 
-# Phi functions compute with the contour trick by kassam
-M = 16
+# Set up to do contour integral trick by Kasam
+M = 20
 r = np.exp(1j*np.pi*(np.arange(1, M+1) - 0.5)/M) #pieces of the unit circle
-
 z = h * (L) #arguments of the phi functions
 LR = z[:, :, None] + r[None, None, :] #take all the points in z and add a unit circle around them
 
-# uses trapezoidal rule over the unit circle to approximate phi
-phi1 = np.mean((np.exp(LR) - 1) / LR, axis=2)
-phi2 = np.mean((np.exp(LR) - 1 - LR) / (LR**2), axis=2)
-phi3 = np.mean((np.exp(LR) - 1 - LR - LR**2/2) / (LR**3), axis=2)
+# the actual coefficents computed using the contour
+f1 = h*np.mean(((-4-LR+np.exp(LR)*(4-3*LR+LR**2))/LR**3),axis=2).real
+f2 = h*np.mean(((2+LR+np.exp(LR)*(-2+LR))/LR**3),axis=2).real
+f3 = h*np.mean(((-4-3*LR-LR**2+np.exp(LR)*(4-LR))/LR**3),axis=2).real
 
-#phi1 = (np.exp(L) - 1) / L
-#phi2 = (np.exp(L) - 1 - L) / (L**2)
-#phi3 = (np.exp(L) - 1 - L - L**2/2) / (L**3)
-
+Q  = h*np.mean(((np.exp(LR/2) - 1) / (LR)), axis=2).real
 
 print("Everything has been precomputed. Moving on to time-stepping.")
+
+# TIME STEPPING =========================================================================================================
 def NonLinear(u_ft): #takes in fourier transformed u and computes the non linear part.
-    u_x = np.fft.ifft2(1j*Kx*u_ft).real
-    u_y = np.fft.ifft2(1j*Ky*u_ft).real
-    return np.fft.fft2(-0.5*(u_x**2 + u_y**2))
+    #spatial derivatives
+    u_x = np.fft.ifft2(-1j*Kx*u_ft).real
+    u_y = np.fft.ifft2(-1j*Ky*u_ft).real
+
+    # nonlinear terms in position space
+    N = -0.5 * (u_x**2 + u_y**2)
+    return np.fft.fft2(N) #return to fourier space
 
 def Step(u_ft):
-    # Stage a
-    a = NonLinear(u_ft)
-
-    # Stage b
-    ua = E2 * (u_ft + 0.5*h*a)
-    b = NonLinear(ua)
-
-    # Stage c
-    ub = E2 * (u_ft + 0.5*h*b)
-    c = NonLinear(ub)
-
-    # Stage d
-    uc = E * (u_ft + h*c)
-    d = NonLinear(uc)
-
-    # Final update
-    u_ft_next = E * u_ft + h * E * (phi1 * a + 2*phi2 * (b + c) + phi3 * d)
-    return u_ft_next
-
-v = v0
-u_list = [u0]
-t_list = [0]
-nplt = 1 #save every nplt steps
-
-for n in range(1,t_steps+1):
+    a00 = u_ft[0,0] #to enforce constant average
     t = n*h
 
+    #RK4 stepping
+    Nu = NonLinear(u_ft)
+    a = E2*u_ft + Q*Nu
+    Na = NonLinear(a)
+    b = E2*u_ft + Q*Na
+    Nb = NonLinear(b)
+    c = E2*a + Q*(2*Nb-Nu)
+    Nc = NonLinear(c)
+    u_ft_new = E*u_ft + Nu*f1 + 2*(Na+Nb)*f2 + Nc*f3
+
+    #enforce constant average
+    u_ft_new[0,0] = a00
+    return u_ft_new
+
+v = np.fft.fft2(u0) #create v as the fourier transform of u0
+
+#start these lists to save the funciton over time
+u_list = [u0]
+t_list = [0]
+
+for n in range(1,t_steps+1):
+    #step
+    t = n*h
     v = Step(v)
-    if n%nplt == 0:
+
+    if n%nplt == 0: #to save for animating
         u = np.fft.ifft2(v).real
         u_list.append(np.copy(u))
         t_list.append(np.copy(t))
+    if n%verbosity == 0: # to output progress
+        print(100*n/len(range(1,t_steps+1)), " % complete")
+    
+# ANIMATING =========================================================================================================
+print("Building animation")
+#max an min for setting plot range
+umax = np.max(np.array(u_list))
+umin = np.min(np.array(u_list))
+print("Maximum: ", umax, " Minimum: ", umin)
 
+# Animation junk
 fig, ax = plt.subplots()
-im = ax.imshow(u_list[0], cmap = "plasma", interpolation='bilinear', extent=[0, h*(N-1), 0, h*(N-1)], origin='lower') #interpolation='bilinear'
+plt.title("2D Kuramoto-Sivashinksy Equation with ETDRK4", y = 1.05)
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+im = ax.imshow(u_list[0], cmap = color, interpolation='bilinear', extent=[0, size, 0, size], origin='lower', vmin = umin, vmax = umax) #interpolation='bilinear'
 
 fig.colorbar(im, ax = ax)
 
@@ -97,5 +142,12 @@ def update(frame):
     return [im]
 
 ani = FuncAnimation(fig, update, frames = len(t_list), interval = 1, blit=True, repeat = True)
-print("Playing Animation.")
-plt.show()
+
+#different choice for the animation
+if save_anim == True:
+    print("Saving Animation.")
+    writervideo = FFMpegWriter(fps=60)
+    ani.save('PDE_Solution.mp4', writer=writervideo)
+else:
+    print("Playing Animation.")
+    plt.show()
